41dc1193a762d4c29e4eaf96ec9b5465080abd34
Salome <sallynanjala254@gmail.com>
2022-09-10 21:04:10 +0300

    Modified my App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import EditTask from './components/EditTask';
 import Task from './components/Task';
-
+import NewTask from './components/NewTask';
+import Search from './components/Search';
+import TaskList from './components/TaskList'
 function App() {
   return (
     <div className="App">
